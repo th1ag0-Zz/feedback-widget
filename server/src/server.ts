ed5049b9c6f9e184de3628aac208535c1eeee027
Thiago Silva <thiago.silva@dlweb.co>
2022-06-03 16:28:44 -0300

    add prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,21 @@
-import express from "express";
+import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("ok");
-  return res.send("ok");
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
-app.listen(3333, () => console.log("Servidor iniciado! 🚀"));
+app.listen(3333, () => console.log('Servidor iniciado! 🚀'));
