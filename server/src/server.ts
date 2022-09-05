d117efbec64c9786cd9b96ef2c6fd59c617f8838
Thiago Silva <thiagof89.tf@gmail.com>
2022-09-05 15:03:06 -0300

    send email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'd68de922f4350a',
+    pass: 'f31fb4de0f26fb',
+  },
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -13,6 +23,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedback Widget <oi@feedbackwidget.com>',
+    to: 'Thiago Silva <thiagodeveloper89@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário do feedback: ${comment}</p>`,
+      `</div>`,
+    ].join(''),
   });
 
   return res.status(201).json({ data: feedback });
